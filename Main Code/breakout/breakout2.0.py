27892c8e462f2ff78fe6de62434749a0eb2ec8b5
khcx4oor <seleregb@gmail.com>
2014-12-12 09:24:04 +0800

    supporting files

--- Main Code/breakout/breakout2.0.py
+++ Main Code/breakout/breakout2.0.py
@@ -87,8 +87,6 @@ class Powerup(pygame.sprite.Sprite):
         elif type == 'multiball':
             self.countdown = 60 * 10
             self.image = pygame.image.load("powerup_ball.png").convert()
-        elif type == 'laser':
-            self.image = pygame.image.load("powerup_laser.png").convert()
         else:
             self.image = pygame.image.load("powerup_lightning.png").convert()
 
@@ -175,7 +173,7 @@ class Game(object):
 
         self.xspeed_init = 3
         self.yspeed_init = 3
-        self.max_lives = 5
+        self.max_lives = 3
         self.paddle_speed = 30
         self.score = 0 
         self.bgcolour = 0x2F, 0x4F, 0x4F  # darkslategrey        
@@ -187,6 +185,7 @@ class Game(object):
         # create our window
         self.window = pygame.display.set_mode(self.size)
 
+        #initalise the paddle and the ball
         self.paddle = Paddle()
         self.ball = Ball()
 
@@ -196,6 +195,7 @@ class Game(object):
         self.pong = pygame.mixer.Sound('Blip_1-Surround-147.wav')
         self.pong.set_volume(10)
 
+        #initalise the wall for building
         self.wall = Wall()
         self.wall.build_wall(self.width)
 
@@ -238,95 +238,29 @@ class Game(object):
             # 60 frames per second
             self.clock.tick(60)
 
-            # process key presses
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-        	            sys.exit()
-                    if event.key == pygame.K_LEFT:                        
-                        self.paddle.paddlerect = self.paddle.paddlerect.move(-self.paddle_speed, 0)
-                        if (self.paddle.paddlerect.left < 0):
-                           self.paddle.paddlerect.left = 0
-                    if event.key == pygame.K_RIGHT:                    
-                        self.paddle.paddlerect = self.paddle.paddlerect.move(self.paddle_speed, 0)
-                        if (self.paddle.paddlerect.right > self.width):
-                            self.paddle.paddlerect.right = self.width
-                    # if entering name, save keys
-                if self.enteringname:
+            #call to handle the game events
+            self.handleEvents()
 
-                    # if backspace, remove last letter until empty string
-                    if event.key == K_BACKSPACE:
-                        self.namesprite.removeLetter()
-
-                    # if enter, self.nameEntered with name
-                    elif event.key == K_RETURN:
-                        self.nameEntered()
-
-                    else:
-                        try:
-                            char = chr(event.key)
-                            # all the characters we want to allow
-                            if str(char) in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_':
-                                self.namesprite.addLetter(char)
-                        except:
-                            pass    # exception from not being able to get char, dont care
-
-
-            # check if bat has hit ball    
-            if self.ball.ballrect.bottom >= self.paddle.paddlerect.top and \
-               self.ball.ballrect.bottom <= self.paddle.paddlerect.bottom and \
-               self.ball.ballrect.right >= self.paddle.paddlerect.left and \
-               self.ball.ballrect.left <= self.paddle.paddlerect.right:
-                self.yspeed = -self.yspeed                
-                self.pong.play(0)                
-                self.offset = self.ball.ballrect.center[0] - self.paddle.paddlerect.center[0]
-                # offset > 0 means ball has hit RHS of bat                   
-                # vary angle of ball depending on where ball hits bat                      
-                if self.offset > 0:
-                    if self.offset > 30:  
-                        self.xspeed = 7
-                    elif self.offset > 23:                 
-                        self.xspeed = 6
-                    elif self.offset > 17:
-                        self.xspeed = 5 
-                else:  
-                    if self.offset < -30:                             
-                        self.xspeed = -7
-                    elif self.offset < -23:
-                        self.xspeed = -6
-                    elif self.xspeed < -17:
-                        self.xspeed = -5
-
-                      
-            # move bat/ball
-            self.ball.ballrect = self.ball.ballrect.move(self.xspeed, self.yspeed)
-
-            if self.ball.ballrect.left < 0 or self.ball.ballrect.right > self.width:
-                self.xspeed = -self.xspeed                
-                self.pong.play(0)            
-            if self.ball.ballrect.top < 0:
-                self.yspeed = -self.yspeed                
-                self.pong.play(0)               
+            #call to handle the movement of the bat/ball
+            self.movement()
 
             # check if ball has gone past bat - lose a life
             if self.ball.ballrect.top > self.height:
                 self.lives -= 1
                 # start a new ball
                 self.xspeed = self.xspeed_init
-                self.rand = random.random()                
+                self.rand = random.random()
                 if random.random() > 0.5:
-                    self.xspeed = -self.xspeed 
-                self.yspeed = self.yspeed_init            
+                    self.xspeed = -self.xspeed
+                self.yspeed = self.yspeed_init
                 self.ball.ballrect.center = self.width * random.random(), self.height / 3
-                if self.lives == 0:                    
+                if self.lives == 0:
                     self.msg = pygame.font.Font(None,70).render("Game Over", True, (0,255,255), self.bgcolour)
                     self.msgrect = self.msg.get_rect()
                     self.msgrect = self.msgrect.move(self.width / 2 - (self.msgrect.center[0]), self.height / 3)
                     self.window.blit(self.msg, self.msgrect)
-                    self.handleHighScores()
                     pygame.display.flip()
+
                     # process key presses
                     #     - ESC to quit
                     #     - any other key to restart game
@@ -338,24 +272,23 @@ class Game(object):
                             if event.type == pygame.KEYDOWN:
                                 if event.key == pygame.K_ESCAPE:
                     	            sys.exit()
-                                if not (event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT):                                    
+                                if not (event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT):
                                     restart = True      
                         if restart:                   
                             self.window.fill(self.bgcolour)
                             self.wall.build_wall(self.width)
                             self.lives = self.max_lives
                             self.score = 0
-                            self.handleHighScores()
                             break
-            
+
             if self.xspeed < 0 and self.ball.ballrect.left < 0:
-                self.xspeed = -self.xspeed                                
+                self.xspeed = -self.xspeed
                 self.pong.play(0)
 
             if self.xspeed > 0 and self.ball.ballrect.right > self.width:
-                self.xspeed = -self.xspeed                               
+                self.xspeed = -self.xspeed
                 self.pong.play(0)
-           
+
             # check if ball has hit wall
             # if yes yhen delete brick and change ball direction
             self.index = self.ball.ballrect.collidelist(self.wall.brickrect)
@@ -364,8 +297,8 @@ class Game(object):
                    self.ball.ballrect.center[0] < self.wall.brickrect[self.index].left:
                     self.xspeed = -self.xspeed
                 else:
-                    self.yspeed = -self.yspeed                
-                self.pong.play(0)              
+                    self.yspeed = -self.yspeed
+                self.pong.play(0)
                 self.wall.brickrect[self.index:self.index + 1] = []
                 self.score += 10
 
@@ -386,7 +319,9 @@ class Game(object):
                 self.window.blit(self.hearts, self.heartsrect)
                 self.livepos+=30
 
+            #call to activate the powerups
             self.managePowerups()
+
             # render our sprites
             # clears the window where the sprites currently are, using the background
             dirty = self.sprites.draw(self.window)
@@ -397,7 +332,6 @@ class Game(object):
                 alive = sprite.update()
                 if alive is False:
                     self.sprites.remove(sprite)
-
 
 
             #build wall
@@ -415,29 +349,67 @@ class Game(object):
             self.window.blit(self.paddle.pimage, self.paddle.paddlerect)
             pygame.display.flip()
 
-            # entering name, render name sprite
-            if self.enteringname:
-                font = pygame.font.Font(None, 35)  # load the default font, size 50
-                color = (255, 50, 0)
-                nameimage = font.render('Enter Name:', True, color)
-                namerect = nameimage.get_rect()
-                namerect.center = 260, 250
-                self.window.blit(nameimage,namerect)
-
-                self.namesprites.clear(self.window, self.bgcolour)
-                dirty = self.namesprites.draw(self.window)
-                pygame.display.update(dirty+[namerect])
-                pygame.display.flip()
-
 
         print 'Quitting. Thanks for playing'
+
+
+    def handleEvents(self):
+        # process key presses
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+        	        sys.exit()
+                if event.key == pygame.K_LEFT:
+                    self.paddle.paddlerect = self.paddle.paddlerect.move(-self.paddle_speed, 0)
+                    if (self.paddle.paddlerect.left < 0):
+                        self.paddle.paddlerect.left = 0
+                if event.key == pygame.K_RIGHT:
+                    self.paddle.paddlerect = self.paddle.paddlerect.move(self.paddle_speed, 0)
+                    if (self.paddle.paddlerect.right > self.width):
+                        self.paddle.paddlerect.right = self.width
+
+    def movement(self):
+        # check if bat has hit ball
+        if self.ball.ballrect.bottom >= self.paddle.paddlerect.top and \
+            self.ball.ballrect.bottom <= self.paddle.paddlerect.bottom and \
+            self.ball.ballrect.right >= self.paddle.paddlerect.left and \
+            self.ball.ballrect.left <= self.paddle.paddlerect.right:
+                self.yspeed = -self.yspeed
+                self.pong.play(0)
+                self.offset = self.ball.ballrect.center[0] - self.paddle.paddlerect.center[0]
+                # offset > 0 means ball has hit RHS of bat
+                # vary angle of ball depending on where ball hits bat
+                if self.offset > 0:
+                    if self.offset > 30:
+                        self.xspeed = 7
+                    elif self.offset > 23:
+                        self.xspeed = 6
+                    elif self.offset > 17:
+                        self.xspeed = 5
+                else:
+                    if self.offset < -30:
+                        self.xspeed = -7
+                    elif self.offset < -23:
+                        self.xspeed = -6
+                    elif self.xspeed < -17:
+                        self.xspeed = -5
+
+        # move bat/ball
+        self.ball.ballrect = self.ball.ballrect.move(self.xspeed, self.yspeed)
+        if self.ball.ballrect.left < 0 or self.ball.ballrect.right > self.width:
+            self.xspeed = -self.xspeed
+            self.pong.play(0)
+        if self.ball.ballrect.top < 0:
+            self.yspeed = -self.yspeed
+            self.pong.play(0)
 
     def managePowerups(self):
         """Called each frame. Drops new powerups as necessary. Checks if the
         paddle hits a powerup and applies the powerup. Manages the powerups
         timing out."""
         # no powerup, update countdown and drop one if necessary
-
 
         if self.currentpowerup is None:
             # decrement powerup drop countdown
@@ -447,12 +419,10 @@ class Game(object):
             if self.powerupdrop <= 0:
                 # drop chances to use with random
                 droppercentages = [
-                (10, '1up'),        # 10% chance
-                (30, 'laser'),      # 20% chance
-                (80, 'multiball'),   # 25% chance
-                (100, 'bigpaddle')  # 45% chance
+                    (30, 'bigpaddle'),
+                    (60, '1up'),
+                    (70, 'multiball')
                 ]
-
 
                 # decide which powerup to drop
                 choice = random.uniform(0,100)
@@ -461,9 +431,7 @@ class Game(object):
                         # create new powerup and add it to render group
                         self.currentpowerup = Powerup(type)
                         self.sprites.add(self.currentpowerup)
-
                         break
-
 
         # if powerup hasn't been collected yet, check for collision
         elif not self.currentpowerup.collected:
@@ -477,17 +445,6 @@ class Game(object):
                 elif self.currentpowerup.type == '1up':
                     # increment player lives
                     self.lives += 1
-
-                elif self.currentpowerup.type == 'multiball':
-                    print "multiball"
-                    """
-                    self.ball2 = Ball()
-                    for i in range(60*100):
-                        print "m1"
-                        #self.ball2.ballrect = self.ball2.ballrect.move(self.width / 2, self.height / 2)
-                        self.ball2.ballrect = self.ball2.ballrect.move(self.xspeed, self.yspeed)
-                        self.window.blit(self.ball2.bimage, self.ball2.ballrect)
-                    """
 
                 #set collected so timer starts
                 self.currentpowerup.collected = True
@@ -512,122 +469,16 @@ class Game(object):
         # powerup is over -- has been collected and countdown <= 0
         else:
 
-
             # if we haven't turned off the current powerup yet, do so
             if self.currentpowerup is not None:
                 if self.currentpowerup.type == 'bigpaddle':
                     self.paddle.shrink()
+
                 # set current to none
                 self.currentpowerup = None
 
-
                 # set new powerupdrop countdown
                 self.powerupdrop = random.randint(60 * 30, 60 * 60)
-
-    def handleHighScores(self):
-        """Called to prompt the user to enter a new
-        high score name and show the high score table."""
-
-        # load high scores
-        highscores = self.parseHighScores()
-
-        # if current score belongs on the table
-        if self.score > int(highscores[-1][1]):
-            # prompt for user name
-            self.enteringname = True
-            self.namesprite = NameSprite( (260, 310) )
-            self.namesprites.add(self.namesprite)
-
-        # otherwise just show table
-        else:
-            self.showHighScores(highscores)
-
-    def nameEntered(self):
-        self.enteringname = False
-        username = self.namesprite.text
-
-        # blit the background onto the window
-        self.window.blit(self.bgcolour, (0,0))
-        # flip the display so the background is on there
-        pygame.display.flip()
-
-        # load high scores
-        highscores = self.parseHighScores()
-
-        # insert the player's score into the highscore table
-        newscores = []
-        for name, score in highscores:
-            if self.score > int(score):
-                newscores.append((username, str(self.score)))
-                self.score = 0    # set to 0 so we dont add it again
-            newscores.append((name, score))
-        newscores = newscores[0:10] # only take 10
-
-        # write scores to high score table
-        highscorefile = 'highscores.txt'
-        f = open(highscorefile, 'w')
-        for name, score in newscores:
-            f.write("%s:%s\n" % (name, score))
-        f.close()
-
-        # display high scores
-        self.showHighScores(newscores)
-
-    def parseHighScores(self):
-        """Parses the high score table and returns a
-        list of scores and their owners"""
-        highscorefile = 'highscores.txt'
-        if os.path.isfile(highscorefile):
-            # read the file into lines
-            f = open(highscorefile, 'r')
-            lines = f.readlines()
-            # break lines into length 2 lists [name, score]
-            scores = []
-            for line in lines:
-                scores.append( line.strip().split(':'))
-            return scores
-        else:
-            # generate default highscore table
-            f = open(highscorefile, 'w')
-            f.write("""JJJ:10000
-III:9000
-HHH:8000
-GGG:7000
-FFF:6000
-EEE:5000
-DDD:4000
-CCC:3000
-BBB:2000
-AAA:1000""")
-            f.close()
-            # call method again - will load the scores we just wrote this time
-            return self.parseHighScores()
-
-    def showHighScores(self, scores):
-        """Draws the high score table onto the screen."""
-        font = pygame.font.Font(None, 35)  # load the default font, size 50
-        color = (255, 50, 0)
-
-        for i in range(len(scores)):
-            name, score = scores[i]
-            # render name
-            nameimage = font.render(name, True, color)
-            namerect = nameimage.get_rect()
-            namerect.left, namerect.y = 40, 100 + (i*(namerect.height + 20))
-            self.window.blit(nameimage,namerect)
-
-            # render score
-            scoreimage = font.render(score, True, color)
-            scorerect = scoreimage.get_rect()
-            scorerect.right, scorerect.y = 480, namerect.y
-            self.window.blit(scoreimage, scorerect)
-
-            # draw dots from name until score
-            for d in range(namerect.right + 25, scorerect.left-10, 25):
-                pygame.draw.rect(self.window, color, pygame.Rect(d, scorerect.centery, 5, 5))
-
-        # flip display
-        pygame.display.flip()
 
 
 # create a game and run it
